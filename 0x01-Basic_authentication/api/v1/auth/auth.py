0148335d05f708181cc00a422d3a22f30e9cbe80
LewisJassy <jassylewis70@gmail.com>
2024-08-06 18:14:51 +0300

    chore: Refactor Auth class to handle excluded paths in require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,26 +7,10 @@ from flask import Flask, request
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Public method to determine if authentication is required
+        Public method that returns False.
+        path and excluded_paths will be used later.
         """
-        if path is None:
-            return True
-
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
-
-        # Ensure the path ends with a slash for comparison
-        if not path.endswith('/'):
-            path += '/'
-
-        for excluded_path in excluded_paths:
-            # Ensure the excluded path ends with a slash for comparison
-            if not excluded_path.endswith('/'):
-                excluded_path += '/'
-            if path == excluded_path:
-                return False
-
-        return True
+        return False
 
     def authorization_header(self, request=None) -> str:
         """
