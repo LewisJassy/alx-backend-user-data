09d3370dd691be39f97e31b5e6cd27e97d0dd47c
LewisJassy <jassylewis70@gmail.com>
2024-08-07 11:30:42 +0300

    Add venv to .gitignore

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+"""Python module"""
+#!/usr/bin/env python3
+
+from typing import List, TypeVar
+from flask import Flask, request
+
+
+class Auth:
+    ''' A Class to manage the API authentication.
+    '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Public method that returns False.
+        path and excluded_paths will be used later.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Public method that returns None.
+        request will be the Flask request object
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Public method that returns None.
+        request will be the Flask request object.
+        """
+        return None
