822d154265eac0f2e069fcc6637d99e3bfd8450b
LewisJassy <jassylewis70@gmail.com>
2024-08-07 15:30:07 +0300

    Refactor authentication API module: improve code structure and readability

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
