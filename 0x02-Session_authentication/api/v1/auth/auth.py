87bfe94878c0ccda4a10ccec894494bc2822a2ed
LewisJassy <jassylewis70@gmail.com>
2024-08-08 20:16:46 +0300

    update app.py

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,28 +3,21 @@
 
 from flask import request
 from typing import List, TypeVar
-import re
+import fnmatch
 
 
 class Auth:
     """authentication class"""
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if a path requires authentication.
-        """
-        if path is not None and excluded_paths is not None:
-            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
-                    return False
+    def require_auth(self, path: str, excluded_paths: list) -> bool:
+        if path is None or excluded_paths is None:
+            return True
+        
+        for excluded_path in excluded_paths:
+            # Check if the excluded path ends with '*'
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+        
         return True
-
-
 
     def authorization_header(self, request=None) -> str:
         """
