cdbcde8442d0c7140c2d237dc0043ffbcd5e7ff8
LewisJassy <jassylewis70@gmail.com>
2024-08-08 09:17:36 +0300

    Refactor session authentication code and fix user authentication logic

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -40,9 +40,9 @@ def authenticate_user():
             if auth.authorization_header(request) is None and \
                     auth.session_cookie(request) is None:
                 abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-    request.current_user = auth.current_user(request)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 
 @app.route('/api/v1/status/', methods=['GET'])
