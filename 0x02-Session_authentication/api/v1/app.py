14840a4aabbed8a3f95c772d3b9536b2206fcc6f
LewisJassy <jassylewis70@gmail.com>
2024-08-10 01:43:42 +0300

    Refactor authenticate_user function to exclude paths with wildcard endings

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,7 +30,8 @@ elif auth_type == "session_auth":
 
 @app.before_request
 def authenticate_user():
-    """Authenticates a user before processing a request."""
+    """Authenticates a user before processing a request.
+    """
     if auth:
         excluded_paths = [
             "/api/v1/status/",
@@ -40,7 +41,8 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             user = auth.current_user(request)
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
