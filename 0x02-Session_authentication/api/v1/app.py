4c953ac506dd9e6d72b5d7d8e5108e3720f64b13
LewisJassy <jassylewis70@gmail.com>
2024-08-10 01:00:17 +0300

    Refactor user authentication logic in app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,9 +47,15 @@ def authenticate_user():
             request.current_user = user
 
 
-@app.route('/api/v1/status', methods=['GET'])
 @app.route('/api/v1/status/', methods=['GET'])
 def status():
+    """Returns a JSON object with a status code of 0."""
+    print("Status route was called")
+    return jsonify({"status": "OK"})
+
+
+@app.route('/api/v1/status', methods=['GET'])
+def status1():
     """Returns a JSON object with a status code of 0."""
     print("Status route was called")
     return jsonify({"status": "OK"})
