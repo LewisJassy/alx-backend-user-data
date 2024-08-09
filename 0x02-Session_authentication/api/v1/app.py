b3afa59d53e3bcb527639692d9b076d042a6ed1e
LewisJassy <jassylewis70@gmail.com>
2024-08-10 01:02:39 +0300

    Refactor user authentication logic in app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,6 +33,7 @@ def authenticate_user():
     """Authenticates a user before processing a request."""
     if auth:
         excluded_paths = [
+            '/api/v1/status',
             "/api/v1/status/",
             "/api/v1/unauthorized/",
             "/api/v1/forbidden/",
@@ -47,15 +48,9 @@ def authenticate_user():
             request.current_user = user
 
 
+@app.route('/api/v1/status', methods=['GET'])
 @app.route('/api/v1/status/', methods=['GET'])
 def status():
-    """Returns a JSON object with a status code of 0."""
-    print("Status route was called")
-    return jsonify({"status": "OK"})
-
-
-@app.route('/api/v1/status', methods=['GET'])
-def status1():
     """Returns a JSON object with a status code of 0."""
     print("Status route was called")
     return jsonify({"status": "OK"})
