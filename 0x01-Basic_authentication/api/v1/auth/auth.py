1727e5631ecd3848a9eec6b2660d98b10d2ddb08
LewisJassy <jassylewis70@gmail.com>
2024-08-11 08:23:15 +0300

    add doc

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,7 @@ import fnmatch
 class Auth:
     """authentication class"""
     def require_auth(self, path: str, excluded_paths: list) -> bool:
+        """check if path requires authentication or not"""
         if path is None or excluded_paths is None:
             return True
         
