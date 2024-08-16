3ac5d2cf76978e66b095437cedc5bbc7f4052e3c
LewisJassy <jassylewis70@gmail.com>
2024-08-16 07:52:05 +0300

    Refactor User class in user.py to update column types and add missing newline at end of file

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -19,10 +19,10 @@ class User(Base):
     """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(Integer, primary_key=True)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
 
     def __repr__(self):
         return f"<User(id={self.id}, email={self.email}, session_id={self.session_id}, reset_token={self.reset_token})>"
