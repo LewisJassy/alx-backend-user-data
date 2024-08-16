451a851f7ee15d2d58acb4fa27780798c1a0ac9b
LewisJassy <jassylewis70@gmail.com>
2024-08-16 08:19:32 +0300

    Refactor add_user method in DB class to use type annotations and update docstring

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,8 +30,8 @@ class DB:
             self.__session = DBSession()
         return self.__session
     
-    def add_user(self, email, hashed_password):
-        """Add a new user to the database
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database.
         """
         try:
             new_user = User(email=email, hashed_password=hashed_password)
